d572849ae34514fb97998de0edef4a9d2ffddb9b
saqib iqbal <saqiboiqbal7772@gmail.com>
2023-12-24 12:06:36 +0500

    email car

---
+++ Detections/email_car.py
@@ -0,0 +1,32 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from pymongo import MongoClient
+from flask import jsonify
+
+def SendMail(ImgFileName, user_email):
+    try:
+        img_data = open(ImgFileName, 'rb').read()
+        msg = MIMEMultipart()
+        msg['Subject'] = 'Vehicle Alert'
+        msg['From'] = 'email.cc'
+        msg['To'] = user_email
+
+        text = MIMEText("Vehcile Crash Detected. Send HELP")
+        msg.attach(text)
+        image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
+        msg.attach(image)
+
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login("email", "password")
+        s.sendmail("email", user_email, msg.as_string())
+        s.quit()
+        print("Email sent successfully!")
+    except Exception as e:
+        print("Error sending email:", e)
+
